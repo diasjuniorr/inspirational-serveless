33b781c31d1a7b74479dbbe55057c1ecb37431ce
Junior <reinaldo.diasjunior@gmail.com>
2021-01-15 11:29:45 -0300

    Attempt to create a lambda which is invoked by a s3 bucket event

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,10 +1,53 @@
-import type { ProxyHandler } from "aws-lambda";
+import AWS from "aws-sdk";
+import type { ProxyHandler, S3Handler } from "aws-lambda";
 import fetch from "node-fetch";
+import sharp from "sharp";
+import { basename, extname } from "path";
 
 interface Quote {
   text: string;
   author: string;
 }
+
+const S3 = new AWS.S3();
+const bucketName = process.env.PictureS3Bucket as string;
+
+export const pictureOptmizer: S3Handler = async (
+  { Records: records },
+  context
+) => {
+  try {
+    await Promise.all(
+      records.map(async (record) => {
+        const { key } = record.s3.object;
+
+        const image = await S3.getObject({
+          Bucket: bucketName,
+          Key: key,
+        }).promise();
+
+        const optimized = await sharp(image.Body as Buffer)
+          .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
+          .toFormat("jpeg", { progressive: true, quality: 50 })
+          .toBuffer();
+
+        await S3.putObject({
+          Body: optimized,
+          Bucket: bucketName,
+          ContentType: "image/jpeg",
+          Key: `compressed/${basename(key, extname(key))}.jpg`,
+        }).promise();
+      })
+    );
+
+    return {
+      statusCode: 201,
+      Body: {},
+    };
+  } catch (err) {
+    return err;
+  }
+};
 
 export const proxyHandler: ProxyHandler = async (event, context) => {
   const { httpMethod, path } = event;
