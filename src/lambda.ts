1367ce1d2e96d7b5e78f8d6dc0134edfef2dbd77
Junior <reinaldo.diasjunior@gmail.com>
2021-01-15 06:49:57 -0300

    Adds /quotes v1.2 lambda

--- src/lambda.ts
+++ src/lambda.ts
@@ -34,27 +34,39 @@ export const proxyHandler: ProxyHandler = async (event, context) => {
       body: "Execute access forbidden",
     };
 };
-
 export const proxyHandler2: ProxyHandler = async (event, context) => {
   const { httpMethod, path } = event;
 
   if (httpMethod === "GET" && ["/quotes"].includes(path)) {
+    let amount = 1;
+    if (event.queryStringParameters && event.queryStringParameters.amount) {
+      amount = +event.queryStringParameters.amount;
+    }
+
     try {
+      const quotes: Quote[] = await fetch(
+        `https://type.fit/api/quotes`
+      ).then((res) => res.json());
+
+      const numberOfQuotes = amount > quotes.length ? quotes.length : amount;
+      const truncatedQuotes = [];
+      for (let i = 0; i < numberOfQuotes; i++) {
+        truncatedQuotes.push(quotes[i]);
+      }
+
       return {
         statusCode: 200,
-        headers: { "content-type": "tex/plain" },
-        body: String("Hello World!"),
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(truncatedQuotes),
       };
     } catch (err) {
-      return {
-        statusCode: 403,
-        body: err,
-      };
+      return err;
     }
   }
 
-  return {
-    statusCode: 403,
-    body: "Execute access forbidden",
-  };
+  if (httpMethod === "PUT" && ["/avatar"])
+    return {
+      statusCode: 403,
+      body: "Execute access forbidden",
+    };
 };
